9e2c783ac947d8fd054152a0ac4e7986152421d3
Wrg1t <Wrg1t@users.noreply.github.com>
2020-08-06 16:41:16 +0800

    master

--- SDQB_http.py
+++ SDQB_http.py
@@ -11,24 +11,26 @@ def server():
     data = request.get_data().decode('utf-8')
     data = loads(data)
     print(data)
-    message = data['message'][0]['data']['text']
-    if 'group_id' in data and '阿巴' in message:
+    message = data['message'] #由于不同版本，返回的数据类型不想类似，根据实际情况自行更改
+    if data['type'] == 'GroupMessage' and '阿巴' in message:
         group = data['group_id']
         abaGPost(group)
-    elif not 'group_id' in data and '阿巴' in message:
+        print(group)
+    elif data['type'] == 'PrivateMessage' and '阿巴' in message:
         user = data['user_id']
         abaPPost(user)
+        print(user)
     return ''
 
 def abaGPost(group):
     content = ''.join(i for i in aba())
     data = {"group_id": group, "message": content}
-    requests.post('http://localhost:5700/send_group_msg', data)
+    requests.post('http://localhost:5700/ws/send_group_msg', data)
 
 def abaPPost(user):
     content = ''.join(i for i in aba())
     data = {"user_id": user, "message": content}
-    requests.post('http://localhost:5700/send_private_msg', data)
+    requests.post('http://localhost:5700/ws/send_private_msg', data)
 
 if __name__ == "__main__":
     bot_server.run(port=5701)
---
+++ SDQB_ws.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_sockets import Sockets
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
+from json import loads, dumps
+from abaGEN import aba
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/api/messages')
+def server(ws):
+    while not ws.closed:
+        data = loads(ws.receive())
+        if data:
+            message = data.get('raw_message')
+            content = ''.join(i for i in aba())
+            if data.get('type') == 'GroupMessage' and '阿巴' in message:
+                msg = {
+                    'action': 'send_group_msg',
+                    'params': {
+                        'group_id': data.get('group_id'),
+                        'message': content
+                    }
+                }
+                print(msg)
+                ws.send(dumps(msg))
+            elif data.get('type') == 'PrivateMessage' and '阿巴' in message:
+                msg = {
+                    "action": "send_private_msg",
+                    "params": {
+                        "user_id": data.get('user_id'),
+                        "message": content
+                    }
+                }
+                print(msg)
+                ws.send(msg)
+
+if __name__ == "__main__":
+    server = pywsgi.WSGIServer(('0.0.0.0', 5701), application=app, handler_class=WebSocketHandler)
+    print('server started')
+    server.serve_forever()
